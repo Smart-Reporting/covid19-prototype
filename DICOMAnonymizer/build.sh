8cea8c2064c09863bda42ad067f359b2ba8eabf4
Stanislav Melnikov <s.melnikov@smartreporting.de>
2020-05-20 15:06:01 +0000

    Fixing module loading

--- DICOMAnonymizer/build.sh
+++ DICOMAnonymizer/build.sh
@@ -2,6 +2,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 BUILD_DIR=./build
+rm -rf "$BUILD_DIR"
+rm ./dist/*
 TOOL_CHAIN_PATH=/emsdk_portable/emscripten/tag-1.39.4/cmake/Modules/Platform/Emscripten.cmake
 if [ ! -d "$BUILD_DIR" ]
 then
